63cb48162a8a873ddc26b3823b747ff6f6333083
John Miller <john2021@gmail.com>
2016-10-29 11:00:08 -0400

    Scaffolding basic project structure

---
+++ BoardGames.Search.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace BoardGames.Search.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
+
+
